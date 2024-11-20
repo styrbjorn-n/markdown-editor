ebb4345de7e9cbb2e6096a2b3e8a7eddbf8ef28e
Styrbjörn Nordberg <styris.n@gmail.com>
2024-11-20 09:48:40 +0100

    initial project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-red-950 font-bold">
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
