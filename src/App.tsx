2e9b5a86e019e19a8876930a0b9d63b0473b2a93
styrbjorn-n <styris.n@gmail.com>
2024-11-26 22:08:23 +0100

    basic ui layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg';
 import { invoke } from '@tauri-apps/api/core';
 import './App.css';
 import { ModeToggle } from './components/mode-toggle';
+import { SidebarTrigger } from './components/ui/sidebar';
+import { Textarea } from './components/ui/textarea';
 
 function App() {
   const [greetMsg, setGreetMsg] = useState('');
@@ -14,46 +16,20 @@ function App() {
   }
 
   return (
-    <main className="container">
-      <h1>Welcome to Tauri + React</h1>
-
-      <div className="flex justify-around">
-        <img
-          src="/vite.svg"
-          className="h-full w-full aspect-square"
-          alt="Vite logo"
-        />
-        <img
-          src="/tauri.svg"
-          className="h-full w-full aspect-square"
-          alt="Tauri logo"
-        />
-        <img
-          src={reactLogo}
-          className="h-full w-full aspect-square"
-          alt="React logo"
-        />
+    <div className="h-full w-full shrink border border-red-600">
+      <div className="flex">
+        {/* header */}
+        <SidebarTrigger />
       </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
-
-      <ModeToggle />
-    </main>
+      <div className="flex w-full justify-center">
+        <div className="w-full max-w-[600px] relative mx-8">
+          <p className="absolute top-[-1.5rem] text-gray-500 font-thin left-0">
+            filename
+          </p>
+          <Textarea />
+        </div>
+      </div>
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from './components/theme-provider';
+import { SidebarProvider, SidebarTrigger } from './components/ui/sidebar';
+import { AppSidebar } from './components/app-sidebar';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider storageKey="vite-ui-theme">
-      <App />
+      <SidebarProvider>
+        <AppSidebar />
+        <main className="w-full">
+          <App />
+        </main>
+      </SidebarProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
