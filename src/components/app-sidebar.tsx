eb5bb82b74f5e1f4e770cb2dceecdbdd76525469
Styrbjörn Nordberg <styris.n@gmail.com>
2024-12-07 19:46:57 +0100

    new file dialog closes on submit

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -5,52 +5,60 @@ import {
   SidebarHeader,
 } from '@/components/ui/sidebar';
 import { invoke } from '@tauri-apps/api/core';
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from './ui/alert-dialog';
+
 import { Input } from './ui/input';
 import { useState } from 'react';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from './ui/dialog';
+import { Button } from './ui/button';
 
 export function AppSidebar() {
   const [newFileName, setNewFileName] = useState('');
+  const [isNewFileOpen, setIsNewFileOpen] = useState(false);
 
   async function newMd(newFileName: String) {
     await invoke('new_md', { newFileName });
-
+    setIsNewFileOpen(false);
     setNewFileName('');
   }
   return (
     <Sidebar>
       <SidebarHeader />
       <SidebarContent>
-        <AlertDialog>
-          <AlertDialogTrigger>new doc</AlertDialogTrigger>
-          <AlertDialogContent>
-            <AlertDialogHeader>
-              <AlertDialogTitle>New Document</AlertDialogTitle>
-            </AlertDialogHeader>
+        <Dialog open={isNewFileOpen} onOpenChange={setIsNewFileOpen}>
+          <DialogTrigger>new doc</DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>New Document</DialogTitle>
+            </DialogHeader>
             <Input
               placeholder="Document Name"
               onChange={(event) => setNewFileName(event.target.value)}
+              value={newFileName}
             />
-            <AlertDialogFooter>
-              <AlertDialogCancel>Cancel</AlertDialogCancel>
-              <AlertDialogAction
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button type="button" variant={'secondary'}>
+                  Cancel
+                </Button>
+              </DialogClose>
+              <Button
+                type="submit"
                 disabled={!newFileName}
                 onClick={() => newMd(newFileName)}
               >
                 Create File
-              </AlertDialogAction>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog>
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </SidebarContent>
       <SidebarFooter />
     </Sidebar>
